b6bfbf2f44729794194d51573af336752209444f
Efren A. Mora <53714269+efrenmo@users.noreply.github.com>
2024-06-23 00:45:01 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,13 @@
 import os
 
+def get_badge_size():
+    try:
+        size = int(os.getenv("INPUT_BADGE_SIZE", "110"))
+        return max(min(size, 300), 50)  # Clamp between 50 and 300
+    except ValueError:
+        print("Warning: Invalid BADGE_SIZE. Using default of 110.")
+        return 110
+
 START_COMMENT = "<!--START_SECTION:badges-->"
 END_COMMENT = "<!--END_SECTION:badges-->"
 
@@ -10,7 +18,8 @@ COMMIT_MESSAGE = os.getenv("INPUT_COMMIT_MESSAGE")
 CREDLY_USER = os.getenv("INPUT_CREDLY_USER")
 CREDLY_SORT = os.getenv("INPUT_CREDLY_SORT")
 
-BADGE_SIZE = os.getenv("INPUT_BADGE_SIZE", "110")
+# BADGE_SIZE = int(os.getenv("INPUT_BADGE_SIZE", "110"))
+BADGE_SIZE = get_badge_size()
 try:
     NUMBER_LAST_BADGES = int(os.getenv("INPUT_NUMBER_LAST_BADGES"))
 except:
