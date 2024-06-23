fabd864f3c38c9b7dc7461ef106cbd1b0a9fd254
Efren A. Mora <53714269+efrenmo@users.noreply.github.com>
2024-06-23 02:03:55 -0400

    Update credly.py

--- services/credly.py
+++ services/credly.py
@@ -43,16 +43,16 @@ class Credly:
     #     }
 
     def convert_to_dict(self, htmlBadge):
-    soupBadge = BeautifulSoup(str(htmlBadge), "lxml")
-    img = soupBadge.findAll("img", {"class": "cr-standard-grid-item-content__image"})[0]
-    original_url = img["src"]
-    resized_url = original_url.replace("110x110", f"{BADGE_SIZE}x{BADGE_SIZE}")
-    return {
-        "title": str(htmlBadge["title"]).replace('"', '\\"'),
-        "href": self.BASE_URL + htmlBadge["href"],
-        "img": resized_url,
-        "original_img": original_url  # Keep the original URL as a fallback
-    }
+        soupBadge = BeautifulSoup(str(htmlBadge), "lxml")
+        img = soupBadge.findAll("img", {"class": "cr-standard-grid-item-content__image"})[0]
+        original_url = img["src"]
+        resized_url = original_url.replace("110x110", f"{BADGE_SIZE}x{BADGE_SIZE}")
+        return {
+            "title": str(htmlBadge["title"]).replace('"', '\\"'),
+            "href": self.BASE_URL + htmlBadge["href"],
+            "img": resized_url,
+            "original_img": original_url  # Keep the original URL as a fallback
+        }
     
     def return_badges_html(self):
         data = self.data_from_html()
